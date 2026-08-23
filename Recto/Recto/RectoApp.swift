5e65730323ca254a39503500abd787abcb56d274
Dusan Zabrodsky <dusan.zabrodsky@gmail.com>
2026-08-23 06:59:05 -0700

    Fix RectoApp.swift: correct Environment scenePhase key path

--- Recto/Recto/RectoApp.swift
+++ Recto/Recto/RectoApp.swift
@@ -3,7 +3,7 @@ import SwiftUI
 @main
 struct RectoApp: App {
     @State private var store = AppStore.load()
-    @Environment(\.(scenePhase) private var scenePhase
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
